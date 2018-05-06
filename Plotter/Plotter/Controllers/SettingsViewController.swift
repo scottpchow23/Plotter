ca62080c58a2ec6bf0eea67a7548d415bfc4ac2b
Scott Chow <tehchowster@gmail.com>
2018-05-06 11:08:02 -0700

    Added Swift Lint and got rid of all of Plotter's warnings.

---
+++ Plotter/Plotter/Controllers/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  Plotter
+//
+//  Created by Scott P. Chow on 2/14/18.
+//  Copyright © 2018 scottpchow. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
